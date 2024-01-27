81cedc5cdb921b0ed0caad474ef778132af5a9e7
shahab-github <113800863+shahab-github@users.noreply.github.com>
2024-01-26 22:51:25 -0700

    feat: added expected output

---
+++ pkg/mygithub/4tpg.go
@@ -0,0 +1,15 @@
+expectedOutput := `[
+  {
+    "cloud": "aws",
+    "services": ["service1", "service2"]
+  },
+  {
+    "cloud": "azure",
+    "services": ["service3", "service4"]
+  },
+  {
+    "cloud": "gcp",
+    "services": ["service5", "service6"]
+  }
+]
+`
